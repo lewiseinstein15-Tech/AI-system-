a9023a0f4a609ce4d3e5f9e611ddd25f4addbd68
lewiseinstein15-Tech <lewiseinstein15@gmail.com>
2026-07-20 02:47:07 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@vercel/sandbox', 'undici'],
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
